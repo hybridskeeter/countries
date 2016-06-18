e07d8d707979a6434faffe34223ded1407f1ec95
Scott Ward <scott@ward-home.co.uk>
2016-06-18 13:41:03 +0100

    Capital of Oz

--- src/countries/Game.java
+++ src/countries/Game.java
@@ -1,10 +1,23 @@
 package countries;
 
+import java.io.Console;
+
 public class Game {
 
-	public static void main(String[] args) {
-		System.out.println("Hello world");
+	private static final String CAPITAL = "Sydney";
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to the capitals game");
+		
+		Console console = System.console();
+		
+		String ans = console.readLine("What is the capital of Australia?", null);
+		
+		if(ans.equalsIgnoreCase(CAPITAL)){
+			System.out.println("Correcto!");
+		}else{
+			System.out.println("No, the answer is " + CAPITAL);
+		}
 	}
 
 }
